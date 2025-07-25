6c9689670bf15f06d09b89c3648c0f2dc7b93ab1
eunchan96 <taegwon02@gmail.com>
2025-07-25 09:20:01 +0900

    feature: weatherInfo에 Getter 생성

--- src/main/java/com/team04/back/domain/weather/weather/entity/WeatherInfo.java
+++ src/main/java/com/team04/back/domain/weather/weather/entity/WeatherInfo.java
@@ -2,6 +2,7 @@ package com.team04.back.domain.weather.weather.entity;
 
 import com.team04.back.domain.weather.weather.enums.Weather;
 import jakarta.persistence.*;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
 @EntityListeners(AuditingEntityListener.class)
 public class WeatherInfo{
     @Id
