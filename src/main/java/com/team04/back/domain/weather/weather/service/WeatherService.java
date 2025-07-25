c76d088319f8a8e52af46ec4c95f215918eca564
eunchan96 <taegwon02@gmail.com>
2025-07-25 14:19:51 +0900

    refactor: WeatherService::save() 복구

--- src/main/java/com/team04/back/domain/weather/weather/service/WeatherService.java
+++ src/main/java/com/team04/back/domain/weather/weather/service/WeatherService.java
@@ -1,16 +1,7 @@
 package com.team04.back.domain.weather.weather.service;
 
-import com.team04.back.domain.weather.weather.entity.WeatherInfo;
-import com.team04.back.domain.weather.weather.repository.WeatherRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class WeatherService {
-    private final WeatherRepository weatherRepository;
-
-    public void save(WeatherInfo weather) {
-        weatherRepository.save(weather);
-    }
 }
